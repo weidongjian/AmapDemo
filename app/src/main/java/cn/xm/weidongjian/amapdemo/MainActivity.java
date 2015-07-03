694d5a30470646d6542406e685e0368d5b5ef77e
weidongjian <weidongjian@gmail.com>
2015-07-03 11:14:55 +0800

    add location function

--- app/src/main/java/cn/xm/weidongjian/amapdemo/MainActivity.java
+++ app/src/main/java/cn/xm/weidongjian/amapdemo/MainActivity.java
@@ -1,15 +1,22 @@
 package cn.xm.weidongjian.amapdemo;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
     private MapView mapView;
     private AMap aMap;
+    private LocationManagerProxy mLocationManagerProxy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mapView.getMap();
         }
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        mLocationManagerProxy = LocationManagerProxy.getInstance(this);
+        mLocationManagerProxy.requestLocationData(
+                LocationProviderProxy.AMapNetwork, 4 * 1000, 15, this);
+        mLocationManagerProxy.setGpsEnable(false);
     }
 
     @Override
@@ -36,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mapView.onPause();
+        deactivate();
     }
 
     @Override
@@ -48,5 +62,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if(aMapLocation != null && aMapLocation.getAMapException().getErrorCode() == 0){
+            //获取位置信息
+            Double geoLat = aMapLocation.getLatitude();
+            Double geoLng = aMapLocation.getLongitude();
+            Log.d("debug", "latitude " + geoLat + aMapLocation.getAddress());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+    public void deactivate() {
+        if (mLocationManagerProxy != null) {
+            mLocationManagerProxy.removeUpdates(this);
+            mLocationManagerProxy.destroy();
+        }
+        mLocationManagerProxy = null;
     }
 }
